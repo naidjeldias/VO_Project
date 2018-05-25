3807c7012af07ef537c0de69e804f9614339199f
Nigel Dias <nigeldias2012@gmail.com>
2018-05-25 20:13:18 -0300

    matriz de calibração
    matriz de calibração

--- visual_odometry.m
+++ visual_odometry.m
@@ -1,7 +1,14 @@
 clear all;
 clc;
 
+%carregando matrizes de projeção
 [cam0, cam1, cam2, cam3] = load_calib_txt();
+
+%computando matrizes intrisecas das câmeras
+intrisic_cam2 = eye(4);
+intrisic_cam3 = eye(4);
+intrisic_cam2(1:3,1:3) = cam2(1:3,1:3);
+intrisic_cam3(1:3,1:3) = cam3(1:3,1:3);
 
 %carregando imagens
 im_left = imread('/media/nigel/Dados/Documents/Projetos/KITTI DATASET/dataset/sequences/00/image_2/000000.png');
@@ -16,8 +23,8 @@ im_left2_gray = rgb2gray(im_left2);
 [matchedPointsA,matchedPointsB] = matching_points_seq_frames(im_left_gray,im_left2_gray);
 
 %apresentando as duas imagens e as features correlacionadas
-figure; showMatchedFeatures(im_left_gray, im_left2_gray, matchedPointsA, matchedPointsB);
-legend('Imagem 1', 'Imagem2');
+%figure; showMatchedFeatures(im_left_gray, im_left2_gray, matchedPointsA, matchedPointsB);
+%legend('Imagem 1', 'Imagem2');
 
 
 
