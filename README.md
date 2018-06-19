7c47cd89c8c3091869d4a0ebc1f719b1f7bb807c
Nigel Dias <nigeldias2012@gmail.com>
2018-06-18 21:22:01 -0300

    Revert "Motion estimation"
    This reverts commit 50ca38c269326c4b1e3440a8542eb0cceade6ce0.

--- README.md
+++ README.md
@@ -1,17 +1,2 @@
-# Descrição
-
-A Odometria Visual é uma área da visão computacional que  tem como principal objectivo estimar a posição de orientação de um agente usando informações provenientes de uma câmera, tendo muitas aplicações nas áreas de robótica móvel, carros autônomos e reconstrução 3D.
-
-Neste projecto é proposto um algoritmo para estimar o movimento de uma câmera estéreo a partir da estimação da pose em dois frames consecutivos.
-
-# Dataset
-Para realização dos testes foram utilizados imagens do Kitti, dois pares, esquerda e direita, de frames consecutivos e um documento txt contendo as matrizes de projeção das câmeras correspondentes a cada par de imagens.
-
-# Detecção e extração de features
-No projeto foi utilizado o detector e descritor de características A-Kaze, proposto recente por F. Alcantarilla. A técnica apresentada apresenta melhor trade-off entre acurácia na estimação de movimento e eficiência computacional em relação aos demais descritores e detectores com SURF, SIFT e ORB.
-
-# Triangulação estéreo
-A informação de profundidade é obtida a partir de triangulação estéreo.
-
-# Estimação de movimento 3D-to-2D
-O movimento é estimado a partir de correspondências 3D para 2D, que consiste em encontrar a matriz de transformação que minimiza o erro de projeção de um ponto 3D para imagem. 
+# VO_Project
+This project consists of a visual odometry implementation
--- compute_homography.m
+++ compute_homography.m
@@ -1,5 +1,5 @@
 
-function [P_mat] = compute_projection_matrix(P,p)
+function [h_mat] = compute_homogra(P,p)
 
     %variaveis de teste
     n = 700;
@@ -8,16 +8,20 @@ function [P_mat] = compute_projection_matrix(P,p)
     x = p(n,:);
     
     %defenindo número de pontos
-    nr_points = size(P,1);
+    nr_points = 900;
     %construindo o sistema linear homogêneo
     for i=1:nr_points
+        %Px_h = P(i,1)/P(i,3);
+        %Py_h = P(i,2)/P(i,3);
+        %A(2*i-1,:) = [Px_h,Py_h,1,0,0,0,-Px_h*p(i,1),-p(i,1)*Py_h,-p(i,1)];
+        %A(2*i, :)  = [0,0,0,Px_h,Py_h,1,-Px_h*p(i,2),-p(i,2)*Py_h,-p(i,2)];
         A(2*i-1,:) = [P(i,1), P(i,2), P(i,3), 1, 0, 0, 0, 0, -P(i,1)*p(i,1), -P(i,2)*p(i,1), -P(i,3)*p(i,1) -p(i,1)];
         A(2*i,:)   = [0, 0, 0, 0, P(i,1), P(i,2), P(i,3), 1, -P(i,1)*p(i,2), -P(i,2)*p(i,2), -P(i,3)*p(i,2) -p(i,2)];
     end
     %resolvendo o sistema usando decomposição singular linear
     [U,S,V] = svd(A);
     %A solução que minimiza A é o
-    %autovetor associado ao menor autovalor
+    %autovetor associado ao menor auto valor
     index = 1;
     smallValue = 999;
     for i=1:size(S,2)
@@ -26,8 +30,10 @@ function [P_mat] = compute_projection_matrix(P,p)
             index = i;
         end
     end
-    %A matriz P é definida pelo autovetor associado ao menor autovalor
+    
     P = V(:,index);
+    
+    %P = P.*-1;
     
     %transformando numa matriz 3x4
     %P_mat = reshape(P,[3,4]);
@@ -36,6 +42,28 @@ function [P_mat] = compute_projection_matrix(P,p)
     
     %reprojetando ponto usando a matriz de projeção
     x_l = P_mat*X';
-        
+    
+    %normalização P_mat
+    P_mat = P_mat*sign(det(P_mat(1:3,1:3)));
+    
+    %[K, Rc_w, Pc, pp, pv] = decomposecamera(P_mat);
+
+    %estimando C
+    [U,S,V] = svd(P_mat);
+    C = V(1:3,end)/V(end,end);
+    
+
+    %decompondo P usando fatorização QR
+    [R,K] = qr(P_mat(1:3,1:3));
+    
+    %garantindo a diagonal positiva de K
+    D = diag(sign(diag(K)));
+    K = K*D;
+    R = D*R;
+    K = K/K(end,end);
+    
+    d = det(R);
+    t = -R*C;
+    
 end
     
